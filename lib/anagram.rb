b408a9e5b6fb52904e6d676852e2324611e13e54
angie <amaticorena103@hotmail.com>
2022-03-24 20:51:28 -0400

    commit tree 526e51b8e544fb2dc2d75f580ffd9aee8667226a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_array)
+        new_words_array = []
+        words_array.map do |words|
+            if words.chars.sort == word.chars.sort
+              new_words_array << words
+            end
+        end
+        new_words_array
+    end
+end
